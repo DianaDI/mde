ec0c5e01d0fae6bb6966e35ad967f5d6dd5f422b
DianaDI <dianke402@gmail.com>
2020-07-27 12:09:29 +0200

    add parser of results

--- src/visualization/open_pc.py
+++ src/visualization/open_pc.py
@@ -3,7 +3,7 @@ from src.data.las2xyz import LasReader
 
 if __name__ == "__main__":
     ## Read .las file
-    inFile = LasReader.read("../../data/KirbyLeafOn2017PointCloudEntireSite_split0_part3_52224_8192.las")
+    inFile = LasReader.read("../../data/KirbyLeafOff2017PointCloudEntireSite_16191_49344.las")
 
     xyz = LasReader.get_scaled_dimensions(inFile)
     inFile.close()
@@ -13,6 +13,6 @@ if __name__ == "__main__":
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(xyz)
 
-    mesh_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(
-        size=10, origin=[min(xyz[:, 0]), min(xyz[:, 1]), min(xyz[:, 2])])
-    o3d.visualization.draw_geometries([pcd, mesh_frame])
+    # mesh_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(
+    #     size=10, origin=[min(xyz[:, 0]), min(xyz[:, 1]), min(xyz[:, 2])])
+    o3d.visualization.draw_geometries([pcd])
