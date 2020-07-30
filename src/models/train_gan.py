1a6bc226040b4d9e67cb14ad16208ca5c52711cd
DianaDI <dianke402@gmail.com>
2020-07-30 10:23:59 +0200

    run74

--- src/models/train_gan.py
+++ src/models/train_gan.py
@@ -7,6 +7,7 @@ from torch.autograd import Variable
 from torchvision import transforms
 from torch.utils.data import DataLoader
 from torch.optim.lr_scheduler import MultiplicativeLR
+import torchvision.utils as vutils
 from src.data.make_dataset import DatadirParser, TrainValTestSplitter, BeraDataset
 from src.data import SPLITS_DIR, DEPTH_MAPS
 from src.models.mde_net import FPNNet
@@ -16,6 +17,7 @@ from src.models.run_params import COMMON_PARAMS, MODEL_SPECIFIC_PARAMS, \
     MODEL_DIR, FIG_SAVE_PATH, FULL_MODEL_SAVING_PATH, RUN_CNT
 from src.models.ssim import SSIM
 from src.models.metrics import root_mean_squared_error
+from src.models.discriminator import Discriminator, weights_init
 
 # set model type
 model_class = FPNNet
@@ -40,7 +42,7 @@ def compute_metrics(output, target):
     rmse = root_mean_squared_error(output, target)
     l1, pixel_losses = L1Loss().forward(output, target)
     print(f'RMSE: {rmse}, L1: {l1}')
-    return rmse, l1.item(), pixel_losses
+    return rmse, l1, pixel_losses
 
 
 def log_sample(cur_batch, plot_every, out, target, inp, edges, pixel_loss, path, epoch, mode):
@@ -68,15 +70,14 @@ def get_prediction(data, model):
     return inp, out, target, edges.detach(), orig_inp.detach()
 
 
-def calc_loss(data, model, l1_criterion, criterion_img, criterion_norm, criterion_ssim, batch_idx, reg=False):
+def calc_loss(data, model, l1_criterion, criterion_img, criterion_norm, batch_idx, reg=False):
     inp, out, target, edges, orig_inp = get_prediction(data, model)
     imgrad_true = imgrad_yx(target, device)
     imgrad_out = imgrad_yx(out, device)
     l1_loss, l1_losses = l1_criterion(out, target, edges, device, factor=params['edge_factor'])
     loss_grad = criterion_img(imgrad_out, imgrad_true)
     loss_normal = criterion_norm(imgrad_out, imgrad_true)
-    #loss_ssim = criterion_ssim(out, target)
-    total_loss = l1_loss + 0.5 * loss_grad + 0.5 * loss_normal #+ 0.5 * loss_ssim
+    total_loss = l1_loss + loss_grad + 0.5 * loss_normal
     if reg:
         loss_reg = Variable(torch.tensor(0.)).to(device)
         for param in model.parameters():
@@ -85,26 +86,12 @@ def calc_loss(data, model, l1_criterion, criterion_img, criterion_norm, criterio
     if batch_idx % 10 == 0:
         print(f'DM l1-loss: {l1_loss.item()}, '
               f'Loss Grad: {loss_grad.item()},  '
-              f'Loss Normal: {loss_normal.item()}, ')
-              #f'Loss SSIM: {loss_ssim.item()}')
+              f'Loss Normal: {loss_normal.item()}')
     return total_loss, l1_losses, out, target, inp, orig_inp, edges
 
 
-def train_on_batch(data, model, l1_criterion, criterion_img, criterion_norm, criterion_ssim, fig_save_path, epoch, batch_idx):
-    loss, l1_losses, out, target, inp, orig_inp, edges = calc_loss(data, model, l1_criterion, criterion_img, criterion_norm, criterion_ssim,
-                                                                   batch_idx)
-    if params['plot_sample']:
-        log_sample(batch_idx, 100, out, target, orig_inp, edges, l1_losses,
-                   fig_save_path, epoch, "train")
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-    return loss.item()
-
-
-def validate_on_batch(data, model, l1_criterion, criterion_img, criterion_norm, criterion_ssim, fig_save_path, epoch, batch_idx):
-    loss, l1_losses, out, target, inp, orig_inp, edges = calc_loss(data, model, l1_criterion, criterion_img, criterion_norm, criterion_ssim,
-                                                                   batch_idx)
+def validate_on_batch(data, model, l1_criterion, criterion_img, criterion_norm, fig_save_path, epoch, batch_idx):
+    loss, l1_losses, out, target, inp, orig_inp, edges = calc_loss(data, model, l1_criterion, criterion_img, criterion_norm, batch_idx)
     if params['plot_sample']:
         log_sample(batch_idx, 100, out, target, orig_inp, edges, l1_losses,
                    fig_save_path, epoch, "validate")
@@ -154,54 +141,138 @@ if __name__ == '__main__':
     model = FPNNet(num_channels=num_channels)
     # Create the Discriminator
     ngpu = torch.cuda.device_count()
+    netD = Discriminator(ngpu)
 
     if params['parallel']:
         if ngpu > 1:
             print(f"Using {ngpu} GPUs")
             model = nn.DataParallel(model, list(range(ngpu)))
+            netD = nn.DataParallel(netD, list(range(ngpu)))
     model.to(device)
+    netD.to(device)
 
     total_params = sum(p.numel() for p in model.parameters())
     print(f'\nNum of parameters: {total_params}')
 
+    # apply the weights_init function to randomly initialize all weights to mean=0, stdev=0.2.
+    netD.apply(weights_init)
+
     l1_criterion = MaskedL1Loss()
     grad_criterion = GradLoss()
     normal_criterion = NormalLoss()
-    ssim = SSIM()
+
+    # Disciminator params
+    # Size of z latent vector (i.e. size of generator input)
+    nz = 512
+    # Size of feature maps in discriminator
+    ndf = 64
+    # Learning rate for optimizers
+    lr_d = 0.0002
+    # Beta1 hyperparam for Adam optimizers
+    beta1_d = 0.5
 
     optimizer = torch.optim.Adam(model.parameters(), lr=params['lr'], weight_decay=4e-5)
+    optimizerD = torch.optim.Adam(netD.parameters(), lr=lr_d, betas=(beta1_d, 0.999))
+
+    criterion_bce = nn.BCELoss()
+
+    # Create batch of latent vectors that we will use to visualize
+    #  the progression of the generator
+    fixed_noise = torch.randn(1, 3, 512, 512, device=device)
+
+    # Establish convention for real and fake labels during training
+    real_label = 1
+    fake_label = 0
+
+    img_list = []
+    G_losses = []
+    D_losses = []
+    iters = 0
+
     lmbda = lambda epoch: params['lr_decay']
     scheduler = MultiplicativeLR(optimizer, lr_lambda=lmbda)
     total_train_loss, total_val_loss = [], []
     first_epoch = 0
     if params['train']:
         print("\nTRAINING STARTING...")
-        if params['load_from_chk']:
-            checkpoint = torch.load(params['chk_point_path'])
-            model.load_state_dict(checkpoint['model_state_dict'])
-            optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-            first_epoch = checkpoint['epoch']
-
         for epoch in range(first_epoch, params['num_epochs']):
             print(f'====== Epoch {epoch} ======')
             train_loss, valid_loss = [], []
             model.train()
             for batch_idx, data in enumerate(train_loader):
-                loss = train_on_batch(data=data, model=model,
-                                      l1_criterion=l1_criterion, criterion_img=grad_criterion, criterion_norm=normal_criterion,
-                                      criterion_ssim=ssim,
-                                      fig_save_path=FIG_SAVE_PATH, epoch=epoch, batch_idx=batch_idx)
-                print(f'Epoch {epoch}, batch_idx {batch_idx} train loss: {loss}')
+                ############################
+                # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
+                ############################
+                ## Train with all-real batch
+                netD.zero_grad()
+                # Format batch
+                dm_inp = Variable(data['depth']).to(device, dtype=torch.float).unsqueeze(1)
+                b_size = dm_inp.size(0)
+                label = torch.full((b_size,), real_label, device=device)
+                # Forward pass real batch through D
+                output = netD(dm_inp).view(-1)
+                # Calculate loss on all-real batch
+                errD_real = criterion_bce(output, label)
+                # Calculate gradients for D in backward pass
+                errD_real.backward()
+                D_x = output.mean().item()
+
+                ## Train with all-fake batch
+                # Generate batch of latent vectors
+                noise = torch.randn(b_size, 3, 512, 512, device=device)
+                # Generate fake image batch with G
+                fake = model(noise)
+                label.fill_(fake_label)
+                # Classify all fake batch with D
+                output = netD(fake.detach()).view(-1)
+                # Calculate D's loss on the all-fake batch
+                errD_fake = criterion_bce(output, label)
+                # Calculate the gradients for this batch
+                errD_fake.backward()
+                D_G_z1 = output.mean().item()
+                # Add the gradients from the all-real and all-fake batches
+                errD = errD_real + errD_fake
+                # Update D
+                optimizerD.step()
+
+                ############################
+                # (2) Update G network: maximize log(D(G(z)))
+                ###########################
+                optimizer.zero_grad()
+                label.fill_(real_label)  # fake labels are real for generator cost
+                # Since we just updated D, perform another forward pass of all-fake batch through D
+                output = netD(fake).view(-1)
+
+                # Calculate G's loss based on this output
+                errG = criterion_bce(output, label)
+                loss, _, _, _, _, _, _ = calc_loss(data, model, l1_criterion, grad_criterion, normal_criterion, batch_idx)
+
+                loss.backward()
+                errG.backward()
+                D_G_z2 = output.mean().item()
+                optimizer.step()
+
+                print(f'Epoch {epoch}, batch_idx {batch_idx} train loss: {loss}, gen-discr loss {D_G_z2}')
+                if batch_idx % 50 == 0:
+                    print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
+                          % (epoch, params['num_epochs'], batch_idx, len(train_loader),
+                             errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
+
                 train_loss.append(loss)
-            if params['save_chk']:
-                save_model_chk(epoch, model, optimizer, f'{MODEL_DIR}/model_chkp_epoch_{epoch}.pth')
+                # Save Losses for plotting later
+                G_losses.append(errG.item())
+                D_losses.append(errD.item())
+                # Check how the generator is doing by saving G's output on fixed_noise
+                # if (batch_idx % 500 == 0) or ((epoch == params['num_epochs'] - 1) and (batch_idx == len(train_loader) - 1)):
+                #     with torch.no_grad():
+                #         fake = model(fixed_noise).detach().cpu()
+                #     img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
 
             model.eval()
             with torch.no_grad():
                 for batch_idx, data in enumerate(val_loader):
                     loss = validate_on_batch(data=data, model=model,
                                              l1_criterion=l1_criterion, criterion_img=grad_criterion, criterion_norm=normal_criterion,
-                                             criterion_ssim=ssim,
                                              fig_save_path=FIG_SAVE_PATH, epoch=epoch, batch_idx=batch_idx)
                     print(f'Epoch {epoch}, batch_idx {batch_idx} val loss: {loss}')
                     valid_loss.append(loss)
@@ -227,12 +298,12 @@ if __name__ == '__main__':
         mre, rmse, l1, l1_range = [], [], [], []
         with torch.no_grad():
             for batch_idx, data in enumerate(test_loader):
-                _, out, target, _, orig_inp = get_prediction(data, model)
+                inp, out, target, edges, orig_inp = get_prediction(data, model)
                 rmse_loss, l1_loss, pixel_losses = compute_metrics(out, target)
                 rmse.append(rmse_loss)
                 l1.append(l1_loss)
                 if params['plot_sample']:
-                    log_sample(batch_idx, 50, out, target, orig_inp, None, pixel_losses, FIG_SAVE_PATH, "", "eval")
+                    log_sample(batch_idx, 50, out, target, orig_inp, edges, pixel_losses, FIG_SAVE_PATH, "", "eval")
                     if batch_idx % 100 == 0:
                         save_dm(out[0][0, :, :].cpu().detach().numpy(), target[0][0, :, :].cpu().detach().numpy(), FIG_SAVE_PATH, batch_idx)
         results = {
